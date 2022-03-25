71088f5b082e582599ac04abe2d9598151d7b4f3
dengriguang <1971167057@qq.com>
2022-03-25 15:08:41 +0800

    refactor: update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,5 +3,13 @@ import App from './App.vue';
 import { setupAxios } from '@openapi/core';
 setupAxios({
   baseURL: 'http://localhost:8080/api',
+  timeout: 5000,
+  request: (config) => {
+    config.headers;
+    return config;
+  },
+  response: (response) => {
+    response.code;
+  },
 });
 createApp(App).mount('#app');
